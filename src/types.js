d4f9c33ab1a788b3fb95fda0d1c475452f045733
Jacob Mischka <jacob@mischka.me>
2018-02-03 00:30:55 -0600

    Improve backdrop, allow track slideshows

--- src/types.js
+++ src/types.js
@@ -1,7 +1,22 @@
 /* @flow */
 
+type SrcImage = {
+	src: string
+};
+
+type SrcsetImage = {
+	srcset: string
+};
+
+type BaseImage = {
+	alt?: string
+};
+
+type Image = BaseImage & (SrcImage | SrcsetImage);
+
 export type Track = {
 	name: string,
 	path: string,
-	image: string
+	image?: Image,
+	images?: Array<Image>
 };
