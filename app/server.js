62101f163b6f9e8bc1f2f0abd007b190766a1747
Jacob Mischka <jacob@mischka.me>
2018-02-21 13:16:26 -0600

    Upgrade sapper

--- app/server.js
+++ app/server.js
@@ -1,12 +1,15 @@
-const app = require('express')();
-const compression = require('compression');
-const sapper = require('sapper');
-const serveStatic = require('serve-static');
+import express from 'express';
+import compression from 'compression';
+import sapper from 'sapper';
+import serveStatic from 'serve-static';
+import fetch from 'node-fetch';
+import { routes } from './manifest/server.js';
+
+const app = express();
 
 const { PORT = 3000 } = process.env;
 
 // this allows us to do e.g. `fetch('/api/blog')` on the server
-const fetch = require('node-fetch');
 global.fetch = (url, opts) => {
 	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
 	return fetch(url, opts);
@@ -17,7 +20,9 @@ app.use(compression({ threshold: 0 }));
 app.use(serveStatic('assets'));
 app.use('/assets', serveStatic('assets'));
 
-app.use(sapper());
+app.use(sapper({
+	routes
+}));
 
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
