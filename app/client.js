7b2a91f70d93f63421cf9b4a1872e1709e545a64
Jacob Mischka <jacob@mischka.me>
2019-03-22 10:11:04 -0500

    Add additional resources, helpful external links

--- app/client.js
+++ app/client.js
@@ -1,5 +1,6 @@
 import { init } from 'sapper/runtime.js';
 import { routes } from './manifest/client.js';
+import '@/polyfills.js';
 
 // `routes` is an array of route objects injected by Sapper
 init(document.querySelector('#sapper'), routes);
---
+++ src/polyfills.js
@@ -0,0 +1,1 @@
+import 'details-element-polyfill';
